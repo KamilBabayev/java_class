790083e9aff618d6e4c160289ee45424f2401a17
Kamil Babayev <kamilyunis@gmail.com>
2024-04-18 16:19:57 +0400

    Task04, get 2 int from console and return multiply result

--- less01/Task03.java
+++ less01/Task03.java
@@ -1,4 +1,14 @@
 package less01;
 
 public class Task03 {
+    public static void main(String[] args) {
+        int x = 48;
+        int y = 4;
+        int result = DivideNumber(x, y);
+        System.out.println(x + " / " + y + " = " + result);
+    }
+
+    public static int DivideNumber(int x, int y) {
+        return  x / y;
+    }
 }
--- less01/Task04.java
+++ less01/Task04.java
@@ -1,2 +1,18 @@
-package less01;public class Task04 {
+package less01;
+
+import java.util.Scanner;
+public class Task04 {
+    public static void main(String[] args) {
+        Menu();
+    }
+
+    public static void Menu() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("enter value of 1st number: ");
+        int x = scanner.nextInt();
+        System.out.println("enter value of 2nd number: ");
+        int y = scanner.nextInt();
+
+        System.out.println(x + " * " + y + " = " + x * y);
+    }
 }
