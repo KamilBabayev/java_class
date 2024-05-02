6d15453598e3dde65aa2ded76fe24629630a674d
Kamil Babayev <kamilyunis@gmail.com>
2024-05-02 09:02:28 +0400

    task5 added

--- less05_strings/Task05.java
+++ less05_strings/Task05.java
@@ -1,10 +1,17 @@
 package less05_strings;
 
-
 // Write a Java program to create a new String object with the contents of a character array
 // (FE: char[] arr_num = new char[] { '1', '2', '3', '4' } --> The book contains 234 pages)
 public class Task05 {
     public static void main(String[] args) {
+        char[] arr_num = new char[] { '1', '2', '3', '4' };
 
+        String pageCount = String.copyValueOf(arr_num, 1, 3);
+        System.out.println(getStringFromCharArr(arr_num));
+    }
+
+    public static String getStringFromCharArr(char[] char_arr) {
+        String pageCount = String.copyValueOf(char_arr, 1, 3);
+        return "The book contains " + pageCount +  " pages.";
     }
 }
